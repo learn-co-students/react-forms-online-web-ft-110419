bf903d1679a0f317ec55b6a1083cf05476d2150d
Gilblasse <gilblasse@gmail.com>
2020-03-19 02:41:37 -0400

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,30 +1,49 @@
-import React from 'react';
+import React from "react";
 
 class Form extends React.Component {
   state = {
     firstName: "John",
     lastName: "Henry"
-  }
+  };
 
   handleFirstNameChange = event => {
     this.setState({
       firstName: event.target.value
-    })
-  }
+    });
+  };
 
   handleLastNameChange = event => {
     this.setState({
       lastName: event.target.value
-    })
-  }
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    let formData = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName
+    };
+    this.sendFormDataSomewhere(formData);
+  };
 
   render() {
     return (
       <form>
-        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-        <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
+        <input
+          type="text"
+          name="firstName"
+          onChange={event => this.handleFirstNameChange(event)}
+          value={this.state.firstName}
+        />
+        <input
+          type="text"
+          name="lastName"
+          onChange={event => this.handleLastNameChange(event)}
+          value={this.state.lastName}
+        />
       </form>
-    )
+    );
   }
 }
 
